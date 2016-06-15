c6912154c6e695d0c5a9b24777bd47b929c0b9cd
William Durand <will+git@drnd.me>
2016-06-15 15:03:12 +0200

    Get rid of the tpl package, let's keep it simple

--- command/generate.go
+++ command/generate.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/TailorDev/msw/parser"
-	"github.com/TailorDev/msw/tpl"
 	"github.com/mitchellh/cli"
 	"github.com/russross/blackfriday"
 )
@@ -40,7 +39,7 @@ func (c *GenerateCommand) Run(args []string) int {
 
 	t, err := template.New("issue").Funcs(template.FuncMap{
 		"markdown": markdown,
-	}).Parse(tpl.IssueHTML)
+	}).Parse(issueHTML)
 	if err != nil {
 		c.UI.Error(fmt.Sprintf("Error parsing template: %s", err))
 		return 1
@@ -81,3 +80,25 @@ func markdown(s string) template.HTML {
 
 	return template.HTML(markdown)
 }
+
+const issueHTML = `
+<h1>Modern Science Weekly &mdash; Issue #{{ .Number }} &mdash; {{ .Date.Format "01/02/2006" }}</h1>
+
+<p style="text-align: justify;">{{ .WelcomeText | markdown }}</p>
+<p>&nbsp;</p>
+
+{{ range $categorie := .Categories }}
+<hr>
+{{ range .Links }}
+<h3 style="margin-top: 2rem;">{{ $categorie.Title }} // <a href="{{ .URL }}">{{ .Name }} &rarr;</a></h3>
+<p style="text-align: justify;">{{ .Abstract | markdown }}</p>
+
+{{ end }}
+{{ end }}
+<p>&nbsp;</p>
+<hr>
+<p style="text-align: justify;">If you received this email directly then you're already signed up, thanks! If however someone forwarded this email to you and you'd like to get it each week then you can subscribe at <a href="https://tinyletter.com/ModernScienceWeekly">https://tinyletter.com/ModernScienceWeekly</a>.</p>
+
+<p style="text-align: center;">
+    <img alt="Modern Science Weekly" class="tl-email-image" data-id="798765" height="100" src="http://gallery.tinyletterapp.com/c66e3e64ae08f8cd0d8e37710e3a5aef07eb6730/images/82443a39-2712-410f-ad7d-632b7fe2f63d.jpg" style="width: 100px; max-width: 100px;" width="100">
+</p>`
